3d73417c1562b5b7db0b019194822d0ef408a105
Zonin <business.kariukimartin@gmail.com>
2023-10-07 08:11:29 +0300

    draw with pyplot

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
 import matplotlib
+import matplotlib.pyplot as plt
+
+
+def run_app():
+    """App entry point"""
+    squares = [1, 4, 9, 16, 25]
+    plt.plot(squares)
+    plt.show()
+
+
+if __name__ == "__main__":
+    run_app()
