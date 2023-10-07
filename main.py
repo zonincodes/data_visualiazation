cfa8a81e9a94a2d1dd10256b277f38b2f9758e6f
Zonin <business.kariukimartin@gmail.com>
2023-10-07 13:32:54 +0300

    remove axis

--- main.py
+++ main.py
@@ -90,9 +90,20 @@ def random_walk():
     while True:
         rw = RandomWalk()
         rw.fill_walk()
+        fig, ax = plt.subplots()
+        point_numbers = tuple(range(rw.num_points))
+        ax.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues, s=15)
+        ax.set_aspect('equal')
 
-        plt.scatter(rw.x_values, rw.y_values, s=15)
-        plt.show()\
+        # Emphasize the first and the last points
+        ax.scatter(0, 0, c='green', edgecolors='none', s=100)
+        ax.scatter(rw.x_values[-1], rw.y_values[-1], c='red', edgecolors='none', s=100)
+
+        # Remove the axes
+        ax.get_xaxis().set_visible(False)
+        ax.get_yaxis().set_visible(False)
+
+        plt.show()
 
         keep_running = input("Make another walk? (Y/N): ")
         if keep_running.lower() == 'n':
